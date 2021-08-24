d8138f65860ac0d37837ae50a7696f901c8ef43c
Dauphine Burns <dauphineburns@gmail.com>
2021-08-23 20:03:56 -0400

    fixed server requires and started api.js

---
+++ Routes/api.js
@@ -0,0 +1,6 @@
+const router = require("express").Router();
+const db = require("../Models/workout");
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -10,13 +10,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require("./Routes/api"));
+app.use(require("./Routes/html"))
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
