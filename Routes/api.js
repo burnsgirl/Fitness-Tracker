30d75f4f124bc80fad542d2e50fbaefbe09fbd3a
Dauphine Burns <dauphineburns@gmail.com>
2021-08-25 17:59:38 -0400

    updated put route

--- Routes/api.js
+++ Routes/api.js
@@ -4,10 +4,13 @@ const Workout = require("../Models/workout");
 //Add exercise to the most recent workout
 router.put("/api/workouts/:id", (req,res) => {
     // findone and update and pass through id to update
-    Workout.findOne(_id)
+    Workout.findOne(
+        params.id, {$push: {exercises:body}}
+    )
 
     .then((data) => res.json(data))
-    .catch((err) => res.json(err));
+    .catch((err => {
+        res.status(400).json(err)}))
 });
 
 //Add new exercise
