b63fe60c047fbb08d804660d8e552bf195b62b52
Dauphine Burns <dauphineburns@gmail.com>
2021-08-25 17:25:39 -0400

    added add new exercise route

--- Routes/api.js
+++ Routes/api.js
@@ -1,6 +1,43 @@
 const router = require("express").Router();
-const db = require("../Models/workout");
+const Workout = require("../Models/workout");
 
+//Add exercise to the most recent workout plan
+router.put("/api/workouts/:id", (req,res) => {
+    // findone and update and pass through id to update
+});
 
+//Add new exercise to a new workout plan
+router.post("/api/workouts", (req, res) => {
+    try {
+        const workoutData = await Workout.create(req.body);
+        res.status(200).json(workoutData)
+    } catch (err) {
+        res.status(404).json(err);
+    }
+});
+
+//View the combined weight of multiple exercises from the past seven workouts on the stats page
+router.get("/api/workouts", (req, res) => {
+    Workout.aggregrate( [
+        {
+            $addFields: {
+                totalWeight: {$sum: "$exercises.weight"}
+            }
+        }
+    ]).limit(7)
+    // .then or try catch
+});
+
+//View the total duration of each workout from the past seven workouts on the stats page
+router.get("/api/workouts/days", (req, res) => {
+    Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: {$sum: "$exercises.duration"}
+            }
+        }
+    ]).limit(7)
+    // .then
+});
 
 module.exports = router;
